f7becfeaf95da341087fad2a84d84e512be3b286
wernerr <rene.werner@verivox.com>
2020-05-18 19:42:48 +0200

    change description

--- utils_bench_test.go
+++ utils_bench_test.go
@@ -25,13 +25,13 @@ func Benchmark_Utils_ToLower(b *testing.B) {
 	var path = "/RePos/GoFiBer/FibEr/iSsues/187643/CoMmEnts"
 	var res string
 
-	b.Run("optimized", func(b *testing.B) {
+	b.Run("fiber ToLower", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = ToLower(path)
 		}
 		AssertEqual(b, "/repos/gofiber/fiber/issues/187643/comments", res)
 	})
-	b.Run("original", func(b *testing.B) {
+	b.Run("strings.ToLower", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = strings.ToLower(path)
 		}
@@ -43,13 +43,13 @@ func Benchmark_Utils_ToLowerBytes(b *testing.B) {
 	var path = []byte("/RePos/GoFiBer/FibEr/iSsues/187643/CoMmEnts")
 	var res []byte
 
-	b.Run("optimized", func(b *testing.B) {
+	b.Run("fiber ToLowerBytes", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = ToLowerBytes(path)
 		}
 		AssertEqual(b, bytes.EqualFold(GetBytes("/repos/gofiber/fiber/issues/187643/comments"), res), true)
 	})
-	b.Run("original", func(b *testing.B) {
+	b.Run("bytes.ToLower", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = bytes.ToLower(path)
 		}
@@ -61,13 +61,13 @@ func Benchmark_Utils_ToUpper(b *testing.B) {
 	var path = "/RePos/GoFiBer/FibEr/iSsues/187643/CoMmEnts"
 	var res string
 
-	b.Run("optimized", func(b *testing.B) {
+	b.Run("fiber ToUpper", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = ToUpper(path)
 		}
 		AssertEqual(b, "/REPOS/GOFIBER/FIBER/ISSUES/187643/COMMENTS", res)
 	})
-	b.Run("original", func(b *testing.B) {
+	b.Run("strings.ToUpper", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = strings.ToUpper(path)
 		}
@@ -80,13 +80,13 @@ func Benchmark_Utils_EqualFolds(b *testing.B) {
 	var right = []byte("/RePos/goFiber/Fiber/issues/187643/COMMENTS")
 	var res bool
 
-	b.Run("optimized", func(b *testing.B) {
+	b.Run("fiber EqualsFold", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = EqualsFold(left, right)
 		}
 		AssertEqual(b, true, res)
 	})
-	b.Run("original", func(b *testing.B) {
+	b.Run("bytes.EqualFold", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = bytes.EqualFold(left, right)
 		}
@@ -96,19 +96,19 @@ func Benchmark_Utils_EqualFolds(b *testing.B) {
 func Benchmark_Utils_Trim(b *testing.B) {
 	var res string
 
-	b.Run("optimized", func(b *testing.B) {
+	b.Run("fiber Trim", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = Trim("  foobar   ", ' ')
 		}
 		AssertEqual(b, "foobar", res)
 	})
-	b.Run("original", func(b *testing.B) {
+	b.Run("strings.Trim", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = strings.Trim("  foobar   ", " ")
 		}
 		AssertEqual(b, "foobar", res)
 	})
-	b.Run("original 2", func(b *testing.B) {
+	b.Run("strings.TrimSpace", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = strings.TrimSpace("  foobar   ")
 		}
@@ -118,13 +118,13 @@ func Benchmark_Utils_Trim(b *testing.B) {
 func Benchmark_Utils_TrimLeft(b *testing.B) {
 	var res string
 
-	b.Run("optimized", func(b *testing.B) {
+	b.Run("fiber TrimLeft", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = TrimLeft("  foobar", ' ')
 		}
 		AssertEqual(b, "foobar", res)
 	})
-	b.Run("original", func(b *testing.B) {
+	b.Run("strings.TrimLeft", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = strings.TrimLeft("  foobar", " ")
 		}
@@ -134,13 +134,13 @@ func Benchmark_Utils_TrimLeft(b *testing.B) {
 func Benchmark_Utils_TrimRight(b *testing.B) {
 	var res string
 
-	b.Run("optimized", func(b *testing.B) {
+	b.Run("fiber TrimRight", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = TrimRight("foobar  ", ' ')
 		}
 		AssertEqual(b, "foobar", res)
 	})
-	b.Run("original", func(b *testing.B) {
+	b.Run("strings.TrimRight", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = strings.TrimRight("foobar  ", " ")
 		}
