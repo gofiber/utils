eedff671d33393689e95795525fe5cba877d2f3f
Fenny <25108519+Fenny@users.noreply.github.com>
2020-05-19 16:10:15 +0200

    Update utils.go

--- utils.go
+++ utils.go
@@ -20,7 +20,7 @@ import (
 const toLowerTable = "\x00\x01\x02\x03\x04\x05\x06\a\b\t\n\v\f\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !\"#$%&'()*+,-./0123456789:;<=>?@abcdefghijklmnopqrstuvwxyz[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u007f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
 const toUpperTable = "\x00\x01\x02\x03\x04\x05\x06\a\b\t\n\v\f\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`ABCDEFGHIJKLMNOPQRSTUVWXYZ{|}~\u007f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
 
-// Tolower ...
+// ToLower is the equivalent of strings.ToLower
 func ToLower(b string) string {
 	var res = make([]byte, len(b))
 	copy(res, b)
@@ -31,7 +31,7 @@ func ToLower(b string) string {
 	return GetString(res)
 }
 
-// ToLowerBytes ...
+// ToLowerBytes is the equivalent of bytes.ToLower
 func ToLowerBytes(b []byte) []byte {
 	for i := 0; i < len(b); i++ {
 		b[i] = toLowerTable[b[i]]
@@ -39,7 +39,7 @@ func ToLowerBytes(b []byte) []byte {
 	return b
 }
 
-// ToUpper ...
+// ToUpper is the equivalent of strings.ToUpper
 func ToUpper(b string) string {
 	var res = make([]byte, len(b))
 	copy(res, b)
@@ -50,7 +50,7 @@ func ToUpper(b string) string {
 	return GetString(res)
 }
 
-// ToUpperBytes ...
+// ToUpperBytes is the equivalent of bytes.ToUpper
 func ToUpperBytes(b []byte) []byte {
 	for i := 0; i < len(b); i++ {
 		b[i] = toUpperTable[b[i]]
@@ -58,7 +58,7 @@ func ToUpperBytes(b []byte) []byte {
 	return b
 }
 
-// TrimRight ...
+// TrimRight is the equivalent of strings.TrimRight
 func TrimRight(s string, cutset byte) string {
 	lenStr := len(s)
 	for lenStr > 0 && s[lenStr-1] == cutset {
@@ -67,7 +67,7 @@ func TrimRight(s string, cutset byte) string {
 	return s[:lenStr]
 }
 
-// TrimLeft
+// TrimLeft is the equivalent of strings.TrimLeft
 func TrimLeft(s string, cutset byte) string {
 	lenStr, start := len(s), 0
 	for start < lenStr && s[start] == cutset {
@@ -76,7 +76,7 @@ func TrimLeft(s string, cutset byte) string {
 	return s[start:]
 }
 
-// Trim ...
+// Trim is the equivalent of strings.Trim
 func Trim(s string, cutset byte) string {
 	i, j := 0, len(s)-1
 	for ; i < j; i++ {
@@ -93,12 +93,26 @@ func Trim(s string, cutset byte) string {
 	return s[i : j+1]
 }
 
-// GetString ...
+// EqualFold the equivalent of bytes.EqualFold
+func EqualsFold(b, s []byte) (equals bool) {
+	n := len(b)
+	equals = n == len(s)
+	if equals {
+		for i := 0; i < n; i++ {
+			if equals = b[i]|0x20 == s[i]|0x20; !equals {
+				break
+			}
+		}
+	}
+	return
+}
+
+// GetString returns a string pointer without allocation
 func GetString(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
 
-// GetBytes ...
+// GetBytes returns a byte pointer without allocation
 func GetBytes(s string) []byte {
 	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
 	bh := reflect.SliceHeader{
@@ -143,7 +157,7 @@ func AssertEqual(t testing.TB, a interface{}, b interface{}, description ...stri
 
 const MIMEOctetStream = "application/octet-stream"
 
-// GetMIME ...
+// GetMIME returns the content-type of a file extension
 func GetMIME(extension string) (mime string) {
 	if len(extension) == 0 {
 		return mime
@@ -159,7 +173,7 @@ func GetMIME(extension string) (mime string) {
 	return mime
 }
 
-// GetTrimmedParam ...
+// GetTrimmedParam trims the ':' & '?' from a string
 func GetTrimmedParam(param string) string {
 	start := 0
 	end := len(param)
@@ -191,20 +205,6 @@ func GetCharPos(s string, char byte, matchCount int) int {
 		matchCount--
 	}
 	return endPos
-}
-
-// equalsFold Bytes compare
-func EqualsFold(b, s []byte) (equals bool) {
-	n := len(b)
-	equals = n == len(s)
-	if equals {
-		for i := 0; i < n; i++ {
-			if equals = b[i]|0x20 == s[i]|0x20; !equals {
-				break
-			}
-		}
-	}
-	return
 }
 
 // GetArgument check if key is in arguments
