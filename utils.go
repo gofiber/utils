5b68e129ef7b0bf7474c9bb56bcd7ca6229f2634
Fenny <25108519+Fenny@users.noreply.github.com>
2020-05-22 12:36:03 +0200

    Update utils.go

--- utils.go
+++ utils.go
@@ -7,6 +7,7 @@ package utils
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -139,11 +140,16 @@ func AssertEqual(t testing.TB, a interface{}, b interface{}, description ...stri
 		bType = reflect.TypeOf(b).Name()
 	}
 
+	testName := "AssertEqual"
+	if t != nil {
+		testName = t.Name()
+	}
+
 	_, file, line, _ := runtime.Caller(1)
 
 	var buf bytes.Buffer
 	w := tabwriter.NewWriter(&buf, 0, 0, 5, ' ', 0)
-	fmt.Fprintf(w, "\nTest:\t%s", t.Name())
+	fmt.Fprintf(w, "\nTest:\t%s", testName)
 	fmt.Fprintf(w, "\nTrace:\t%s:%d", filepath.Base(file), line)
 	fmt.Fprintf(w, "\nError:\tNot equal")
 	fmt.Fprintf(w, "\nExpect:\t%v\t[%s]", a, aType)
@@ -153,11 +159,16 @@ func AssertEqual(t testing.TB, a interface{}, b interface{}, description ...stri
 		fmt.Fprintf(w, "\nDescription:\t%s", description[0])
 	}
 
-	err := w.Flush()
-	if err != nil {
-		t.Fatal(err)
+	result := ""
+	if err := w.Flush(); err != nil {
+		result = err.Error()
 	} else {
-		t.Fatal(buf.String())
+		result = buf.String()
+	}
+	if t != nil {
+		t.Fatal(result)
+	} else {
+		log.Fatal(result)
 	}
 }
 
