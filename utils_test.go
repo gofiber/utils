7a8ac69cad116368e72320bdc04f81942260ee3e
Fenny <25108519+Fenny@users.noreply.github.com>
2020-06-03 15:46:08 +0200

    Add UUID test

--- utils_test.go
+++ utils_test.go
@@ -9,6 +9,12 @@ import (
 	"testing"
 )
 
+func Test_Utils_UUID(t *testing.T) {
+	t.Parallel()
+	res := UUID()
+	AssertEqual(t, 36, len(res))
+}
+
 func Test_Utils_ToUpper(t *testing.T) {
 	t.Parallel()
 	res := ToUpper("/my/name/is/:param/*")
