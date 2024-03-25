3717ffbb212199476ddeeefef43bace35a1df96f
Jason McNeil <sixcolors@mac.com>
2024-03-25 12:37:01 -0300

    feat: Add IfToLower and IfToUpper functions for converting ASCII strings to lowercase and uppercase respectively

--- strings_test.go
+++ strings_test.go
@@ -30,6 +30,18 @@ func Benchmark_ToUpper(b *testing.B) {
 		}
 		require.Equal(b, upperStr, res)
 	})
+	b.Run("IfToUpper-Upper", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			res = IfToUpper(upperStr)
+		}
+		require.Equal(b, upperStr, res)
+	})
+	b.Run("IfToUpper-Mixed", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			res = IfToUpper(largeStr)
+		}
+		require.Equal(b, upperStr, res)
+	})
 	b.Run("default", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = strings.ToUpper(largeStr)
@@ -55,10 +67,61 @@ func Benchmark_ToLower(b *testing.B) {
 		}
 		require.Equal(b, lowerStr, res)
 	})
+	b.Run("IfToLower-Lower", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			res = IfToLower(lowerStr)
+		}
+		require.Equal(b, lowerStr, res)
+	})
+	b.Run("IfToLower-Mixed", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			res = IfToLower(largeStr)
+		}
+		require.Equal(b, lowerStr, res)
+	})
 	b.Run("default", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			res = strings.ToLower(largeStr)
 		}
 		require.Equal(b, lowerStr, res)
+	})
+}
+
+func Test_IfToUpper(t *testing.T) {
+	t.Parallel()
+	require.Equal(t, "MYNAMEISPARAM", IfToUpper("MYNAMEISPARAM")) // already uppercase
+	require.Equal(t, "MYNAMEISPARAM", IfToUpper("mynameisparam")) // lowercase to uppercase
+	require.Equal(t, "MYNAMEISPARAM", IfToUpper("MyNameIsParam")) // mixed case
+}
+
+func Test_IfToLower(t *testing.T) {
+	t.Parallel()
+	require.Equal(t, "mynameisparam", IfToLower("mynameisparam"))           // already lowercase
+	require.Equal(t, "mynameisparam", IfToLower("myNameIsParam"))           // mixed case
+	require.Equal(t, "https://gofiber.io", IfToLower("https://gofiber.io")) // Origin Header Type URL
+	require.Equal(t, "mynameisparam", IfToLower("MYNAMEISPARAM"))           // uppercase to lowercase
+}
+
+// Benchmark_IfToLower_HeadersOrigin benchmarks the IfToLower function with an origin header type URL.
+// These headers are typically lowercase, so the function should return the input string without modification.
+func Benchmark_IfToToLower_HeadersOrigin(b *testing.B) {
+	var res string
+	b.Run("fiber", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			res = ToLower("https://gofiber.io")
+		}
+		require.Equal(b, "https://gofiber.io", res)
+	})
+	b.Run("IfToLower-Lower", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			res = IfToLower("https://gofiber.io")
+		}
+		require.Equal(b, "https://gofiber.io", res)
+	})
+	b.Run("default", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			res = strings.ToLower("https://gofiber.io")
+		}
+		require.Equal(b, "https://gofiber.io", res)
 	})
 }
