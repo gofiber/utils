f4095c73c49d7b47e0dd34e739ad8869fc05f4e2
René <rene@gofiber.io>
2025-12-04 10:39:33 +0100

    optimize TrimSpace

--- byteseq.go
+++ byteseq.go
@@ -92,35 +92,15 @@ func TrimRight[S byteSeq](s S, cutset byte) S {
 // This is an optimized version that's faster than strings/bytes.TrimSpace for ASCII strings.
 // It removes the following ASCII whitespace characters: space, tab, newline, carriage return, vertical tab, and form feed.
 func TrimSpace[S byteSeq](s S) S {
-	length := len(s)
+	i, j := 0, len(s)-1
 
-	// Fast path for empty input
-	if length == 0 {
-		return s
+	// Find first non-whitespace from start
+	for ; i <= j && (s[i] == ' ' || s[i]-'\t' <= 4); i++ {
 	}
 
-	// Find first non-whitespace character
-	start := 0
-	for start < length && whitespaceTable[s[start]] {
-		start++
+	// Find first non-whitespace from end
+	for ; i < j && (s[j] == ' ' || s[j]-'\t' <= 4); j-- {
 	}
 
-	// If all whitespace, return empty with zero capacity to match bytes.TrimSpace behavior
-	if start == length {
-		return s[length:length]
-	}
-
-	// Find last non-whitespace character
-	end := length - 1
-	for end > start && whitespaceTable[s[end]] {
-		end--
-	}
-
-	// If no trimming needed, return original (no allocation)
-	if start == 0 && end == length-1 {
-		return s
-	}
-
-	// Return trimmed substring/subslice
-	return s[start : end+1]
+	return s[i : j+1]
 }
