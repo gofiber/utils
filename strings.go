163a536a62efe5445ecdf0ed8561252a6bf43ab2
René <rene@gofiber.io>
2025-12-05 12:32:03 +0100

    optimize ToLower and ToUpper functions for improved performance and reduced allocations

--- strings.go
+++ strings.go
@@ -6,22 +6,45 @@ package utils
 
 // ToLower converts ascii string to lower-case
 func ToLower(b string) string {
-	res := make([]byte, len(b))
-	copy(res, b)
-	for i := range res {
-		res[i] = toLowerTable[res[i]]
+	if len(b) == 0 {
+		return b
 	}
 
-	return UnsafeString(res)
+	for i := 0; i < len(b); i++ {
+		c := b[i]
+		low := toLowerTable[c]
+		if low != c {
+			res := make([]byte, len(b))
+			copy(res, b[:i])
+			res[i] = low
+			for j := i + 1; j < len(b); j++ {
+				res[j] = toLowerTable[b[j]]
+			}
+			return UnsafeString(res)
+		}
+	}
+	return b
 }
 
 // ToUpper converts ascii string to upper-case
 func ToUpper(b string) string {
-	res := make([]byte, len(b))
-	copy(res, b)
-	for i := range res {
-		res[i] = toUpperTable[res[i]]
+	if len(b) == 0 {
+		return b
 	}
 
-	return UnsafeString(res)
+	for i := 0; i < len(b); i++ {
+		c := b[i]
+		up := toUpperTable[c]
+		if up != c {
+			res := make([]byte, len(b))
+			copy(res, b[:i])
+			res[i] = up
+			for j := i + 1; j < len(b); j++ {
+				res[j] = toUpperTable[b[j]]
+			}
+			return UnsafeString(res)
+		}
+	}
+
+	return b
 }
